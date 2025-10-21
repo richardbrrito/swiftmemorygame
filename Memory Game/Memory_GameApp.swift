cd005b585b197658ce282ad68bbf2182a11f7a26
Richard Brito <richardbrito116@gmail.com>
2025-10-21 16:26:24 -0400

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Richard Brito on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memory GameTests/Memory_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameTests.swift
+//  Memory GameTests
+//
+//  Created by Richard Brito on 10/21/25.
+//
+
+import Testing
+@testable import Memory_Game
+
+struct Memory_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
